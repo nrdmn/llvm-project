9f9843b1242bc15e7e7e2bd5ea6ab1f52d75616e
Nick Erdmann <nick.erdmann@rwth-aachen.de>
2024-02-01 20:46:22 +0100

    stm8: Start of STM8 project
    This is the first commit of our STM8 project with a few files copied
    from other targets.

---
+++ llvm/lib/Target/STM8/STM8TargetMachine.h
@@ -0,0 +1,25 @@
+//===-- STM8TargetMachine.h - Define TargetMachine for STM8 -----*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the STM8 specific subclass of TargetMachine.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_STM8_TARGET_MACHINE_H
+#define LLVM_STM8_TARGET_MACHINE_H
+
+#include "llvm/IR/DataLayout.h"
+#include "llvm/Target/TargetMachine.h"
+
+#include <optional>
+
+namespace llvm {
+
+} // end namespace llvm
+
+#endif // LLVM_STM8_TARGET_MACHINE_H
---
+++ llvm/lib/Target/STM8/TargetInfo/STM8TargetInfo.cpp
@@ -0,0 +1,13 @@
+//===-- STM8TargetInfo.cpp - STM8 Target Implementation -------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "TargetInfo/STM8TargetInfo.h"
+#include "llvm/MC/TargetRegistry.h"
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSTM8TargetInfo() {
+}
