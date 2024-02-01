dc897d781fe37e3624a30d19bbe094304c855f47
Nick Erdmann <nick.erdmann@rwth-aachen.de>
2024-02-01 20:46:22 +0100

    Start of STM8 project

---
+++ llvm/lib/Target/STM8/MCTargetDesc/STM8MCTargetDesc.h
@@ -0,0 +1,16 @@
+//===-- STM8MCTargetDesc.h - STM8 Target Descriptions -----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides STM8 specific target descriptions.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_STM8_MCTARGET_DESC_H
+#define LLVM_STM8_MCTARGET_DESC_H
+
+#endif // LLVM_STM8_MCTARGET_DESC_H
