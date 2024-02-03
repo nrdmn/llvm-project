df796cd884209bd3607a47a17da204f42244a032
Sophie Tyalie <dev@flowerpot.me>
2024-02-03 07:47:14 +0100

    stm8: Add basic target description
    The goal here was mostly, trying to get it to compile after adding the
    `STM8RegisterInfo.{h,cpp}` files. This required adding the machine
    description autogen to the build and a few other classes that are being
    used through. Although there are mostly in skeletal form currently.
    
    Signed-off-by: Sophie Tyalie <dev@flowerpot.me>

---
+++ llvm/lib/Target/STM8/STM8ISelLowering.h
@@ -0,0 +1,39 @@
+//===-- STM8ISelLowering.h - STM8 DAG Lowering Interface ------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the interfaces that STM8 uses to lower LLVM code into a
+// selection DAG.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_STM8_ISEL_LOWERING_H
+#define LLVM_STM8_ISEL_LOWERING_H
+
+#include "llvm/CodeGen/CallingConvLower.h"
+#include "llvm/CodeGen/TargetLowering.h"
+
+namespace llvm {
+
+namespace STM8ISD {
+
+/// STM8 specific DAG nodes
+enum NodeType {
+  /// Start the numbering where the builtin ops leave off.
+  FIRST_NUMBER = ISD::BUILTIN_OP_END,
+  /// 
+};
+
+} // end of namespace STM8ISD
+
+class STM8TargetLowering : public TargetLowering {
+};
+
+} // end of namespace llvm
+
+#endif // LLVM_STM8_ISEL_LOWERING_H
+
---
+++ llvm/lib/Target/STM8/STM8RegisterInfo.cpp
@@ -0,0 +1,30 @@
+//===-- STM8RegisterInfo.cpp - STM8 Register Information --------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains the STM8 implementation of the TargetRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "STM8RegisterInfo.h"
+
+#include "llvm/ADT/BitVector.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+#include "llvm/CodeGen/MachineRegisterInfo.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/IR/Function.h"
+
+#include "MCTargetDesc/STM8MCTargetDesc.h"
+
+#define GET_REGINFO_TARGET_DESC
+#include "STM8GenRegisterInfo.inc"
+
+namespace llvm {
+
+} // end of namespace llvm
