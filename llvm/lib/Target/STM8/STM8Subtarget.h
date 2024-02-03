43519a2722fed09f40e6d94f8408bf306fe9c1b8
Sophie Tyalie <dev@flowerpot.me>
2024-02-03 07:47:14 +0100

    stm8: Add basic target description
    The goal here was mostly, trying to get it to compile after adding the
    `STM8RegisterInfo.{h,cpp}` files. This required adding the machine
    description autogen to the build and a few other classes that are being
    used through. Although there are mostly in skeletal form currently.
    
    Signed-off-by: Sophie Tyalie <dev@flowerpot.me>

---
+++ llvm/lib/Target/STM8/STM8Subtarget.h
@@ -0,0 +1,43 @@
+//===-- STM8Subtarget.h - Define Subtarget for the STM8 ---------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the STM8 specific subclass of TargetSubtargetInfo.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_STM8_SUBTARGET_H
+#define LLVM_STM8_SUBTARGET_H
+
+#include "llvm/CodeGen/TargetSubtargetInfo.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/Target/TargetMachine.h"
+
+#include "STM8FrameLowering.h"
+#include "STM8ISelLowering.h"
+
+#define GET_SUBTARGETINFO_HEADER
+#include "STM8GenSubtargetInfo.inc"
+
+namespace llvm {
+/// A specific STM8 target MCU
+class STM8Subtarget : public STM8GenSubtargetInfo {
+  STM8FrameLowering FrameLowering;
+  STM8TargetLowering TLInfo;
+public:
+  
+  const STM8FrameLowering *getFrameLowering() const override {
+    return &FrameLowering;
+  }
+  const STM8TargetLowering *getTargetLowering() const override {
+    return &TLInfo;
+  }
+};
+
+}
+
+#endif // !LLVM_STM8_SUBTARGET_H
