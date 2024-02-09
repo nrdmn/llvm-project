bbdfd1f6e7e68556303ec649ba9f991bd8081ed8
Sophie Tyalie <dev@flowerpot.me>
2024-02-09 22:42:46 +0100

    [wip] stm8: Register instruction info + skeleton
    Adds simple instruction skeleton and registers the generated instruction
    info class to the target registry.
    
    Currently only NOP has been implemented to calm the instruction info
    table-gen, which would complain without at least one existing instruction.
    
    It needs to be noted, that one can presumably create a more sensible
    instruction format classification than what has currently been started
    with in `STM8InstrInfo.td`. Maybe a look into the stm8-binutils fork
    under opcodes/stm8-* might help here.
    
    Signed-off-by: Sophie Tyalie <dev@flowerpot.me>

--- llvm/lib/Target/STM8/MCTargetDesc/STM8MCTargetDesc.cpp
+++ llvm/lib/Target/STM8/MCTargetDesc/STM8MCTargetDesc.cpp
@@ -14,9 +14,13 @@
 #include "STM8MCAsmInfo.h"
 #include "TargetInfo/STM8TargetInfo.h"
 
+#include "llvm/MC/MCInstrInfo.h"
 #include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/TargetRegistry.h"
 #include "llvm/MC/MCAsmBackend.h"
+
+#define GET_INSTRINFO_MC_DESC
+#include "STM8GenInstrInfo.inc"
 
 #define GET_REGINFO_MC_DESC
 #include "STM8GenRegisterInfo.inc"
@@ -30,6 +34,12 @@ static MCAsmInfo *createSTM8MCAsmInfo(const MCRegisterInfo &MRI,
   return MAI;
 }
 
+static MCInstrInfo *createSTM8MCInstrInfo() {
+  MCInstrInfo *X = new MCInstrInfo();
+  InitSTM8MCInstrInfo(X);
+  return X;
+}
+
 static MCRegisterInfo *createSTM8MCRegisterInfo(const Triple &TT) {
   MCRegisterInfo *X = new MCRegisterInfo();
   InitSTM8MCRegisterInfo(X, 0);
@@ -40,6 +50,10 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSTM8TargetMC() {
   // Register the MC asm info
   TargetRegistry::RegisterMCAsmInfo(getTheSTM8Target(), createSTM8MCAsmInfo);
 
+  // Register the MC instruction info
+  TargetRegistry::RegisterMCInstrInfo(getTheSTM8Target(), createSTM8MCInstrInfo);
+
   // Register the MC register info
   TargetRegistry::RegisterMCRegInfo(getTheSTM8Target(), createSTM8MCRegisterInfo);
+
 }
