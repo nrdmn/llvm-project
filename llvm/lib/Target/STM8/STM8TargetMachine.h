c2e2e6a9c27d08f1f74fe30a33159238e5eb7efd
Nick Erdmann <nick.erdmann@rwth-aachen.de>
2024-02-13 01:38:24 +0100

    stm8: clean up
    Clean up stuff that we either don't understand yet or that we do not
    need.
    
    Originally from ca8b506166977fda6a85c8103eb63062f626c18d.

--- llvm/lib/Target/STM8/STM8TargetMachine.h
+++ llvm/lib/Target/STM8/STM8TargetMachine.h
@@ -20,32 +20,16 @@
 
 namespace llvm {
 
-class Module;
-
-struct STM8InstrInfo {};
-struct TargetFrameInfo {};
-
 class STM8TargetMachine : public LLVMTargetMachine {
-  STM8InstrInfo InstrInfo;
-  TargetFrameInfo FrameInfo;
-
 public:
   STM8TargetMachine(const Target &T, const Triple &TT, StringRef CPU,
                     StringRef FS, const TargetOptions &Options,
                     std::optional<Reloc::Model> RM, std::optional<CodeModel::Model> CM,
                     CodeGenOptLevel OL, bool JIT);
-  ~STM8TargetMachine() override = default;
 
-  virtual const STM8InstrInfo *getInstrInfo() const { return &InstrInfo; }
-  virtual const TargetFrameInfo *getFrameInfo() const { return &FrameInfo; }
   const STM8Subtarget *getSubtargetImpl(const Function &) const override { return nullptr; }
-  virtual const TargetRegisterInfo *getRegisterInfo() const { return nullptr; }
-  virtual const class DataLayout *getDataLayout() const { return nullptr; }
 
   // Pass Pipeline Configuration
-  virtual bool addInstSelector(PassManagerBase &PM, bool Fast) { return false; };
-  virtual bool addPreEmitPass(PassManagerBase &PM, bool Fast) { return false; };
-
   TargetPassConfig *createPassConfig(PassManagerBase &PM) override;
 };
 
