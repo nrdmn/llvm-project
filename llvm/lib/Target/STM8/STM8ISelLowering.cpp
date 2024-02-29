a01ed1510dd41ee5d2a6342aca233d52d946dde1
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-02-29 15:05:29 -0600

    stm8: implement CanLowerReturn
    Using the previously implemented calling convention for return values,
    we can check for a function whether it can be lowered for our target
    using `CanLowerReturn`.
    
    Luckily most of the helper methods are already part of LLVM
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- llvm/lib/Target/STM8/STM8ISelLowering.cpp
+++ llvm/lib/Target/STM8/STM8ISelLowering.cpp
@@ -61,7 +61,9 @@ bool STM8TargetLowering::CanLowerReturn(
     CallingConv::ID CallConv, MachineFunction &MF, bool IsVarArg,
     const SmallVectorImpl<ISD::OutputArg> &Outs, LLVMContext &Context) const {
 
-  return true;
+  SmallVector<CCValAssign, 16> RVLocs;
+  CCState CCInfo(CallConv, IsVarArg, MF, RVLocs, Context);
+  return CCInfo.CheckReturn(Outs, RetCC_STM8);
 }
 
 SDValue
