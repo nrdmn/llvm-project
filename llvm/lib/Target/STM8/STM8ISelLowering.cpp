c45563d544b1ad500a58c734796818a5e6d16454
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2024-02-29 14:50:08 -0600

    stm8: Make STM8TargetLowering non-abstract
    Previously not all required methods of STM8TargetLowering were
    overriden, making the class abstract in turn. This resulted in a few
    unexpected crashes early on.
    
    This patch removes this limitation even if the results are currently
    non-sensical.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- llvm/lib/Target/STM8/STM8ISelLowering.cpp
+++ llvm/lib/Target/STM8/STM8ISelLowering.cpp
@@ -10,14 +10,49 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "STM8.h"
+#include "STM8RegisterInfo.h"
 #include "STM8ISelLowering.h"
 #include "STM8TargetMachine.h"
+#include "MCTargetDesc/STM8MCTargetDesc.h"
+
+using namespace llvm;
 
 namespace llvm {
 
 STM8TargetLowering::STM8TargetLowering(const STM8TargetMachine &TM,
                                        const STM8Subtarget &STI)
     : TargetLowering(TM), Subtarget(STI) {}
+//===----------------------------------------------------------------------===//
+//                      Calling Convention Implementation
+//===----------------------------------------------------------------------===//
+
+#include "STM8GenCallingConv.inc"
+
+SDValue STM8TargetLowering::LowerFormalArguments(
+    SDValue Chain, CallingConv::ID CallConv, bool isVarArg,
+    const SmallVectorImpl<ISD::InputArg> &Ins, const SDLoc &dl,
+    SelectionDAG &DAG, SmallVectorImpl<SDValue> &InVals) const {
+
+  return Chain;
+}
+
+bool STM8TargetLowering::CanLowerReturn(
+    CallingConv::ID CallConv, MachineFunction &MF, bool IsVarArg,
+    const SmallVectorImpl<ISD::OutputArg> &Outs, LLVMContext &Context) const {
+
+  return true;
+}
+
+SDValue
+STM8TargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
+                                bool isVarArg,
+                                const SmallVectorImpl<ISD::OutputArg> &Outs,
+                                const SmallVectorImpl<SDValue> &OutVals,
+                                const SDLoc &dl, SelectionDAG &DAG) const {
+
+  return Chain;
+}
 
 MVT STM8TargetLowering::getPointerTy(const DataLayout &DL, unsigned AS) const {
   if (AS == 0 && DL.getPointerSizeInBits(AS) == 24)
